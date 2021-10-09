9ee170378e26d7eb0bf1301ce4f1fb78026ca543
Sithira Munasinghe <sithiraac@gmail.com>
2021-10-09 20:18:53 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/me/sithiramunasinghe/notes_buddy/notes_buddy/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.sithiramunasinghe.notes_buddy.notes_buddy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/note_detail.dart
@@ -0,0 +1,281 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:notes_buddy/db_helper/db_helper.dart';
+import 'package:notes_buddy/modal_class/notes.dart';
+import 'package:notes_buddy/utils/widgets.dart';
+
+class NoteDetail extends StatefulWidget {
+  final String appBarTitle;
+  final Note note;
+
+  const NoteDetail({Key key, this.note, this.appBarTitle}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return NoteDetailState();
+  }
+}
+
+class NoteDetailState extends State<NoteDetail> {
+  DatabaseHelper helper = DatabaseHelper();
+
+  String appBarTitle;
+  Note note;
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  int color;
+  bool isEdited = false;
+
+  @override
+  void initState() {
+    super.initState();
+    appBarTitle = widget.appBarTitle;
+    note = widget.note;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    titleController.text = note.title;
+    descriptionController.text = note.description;
+    color = note.color;
+    return WillPopScope(
+        onWillPop: () async {
+          isEdited ? showDiscardDialog(context) : moveToLastScreen();
+          return false;
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            title: Text(
+              widget.appBarTitle,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            backgroundColor: colors[color],
+            leading: IconButton(
+                icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+                onPressed: () {
+                  isEdited ? showDiscardDialog(context) : moveToLastScreen();
+                }),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(
+                  Icons.save,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  titleController.text.isEmpty
+                      ? showEmptyTitleDialog(context)
+                      : _save();
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.delete, color: Colors.black),
+                onPressed: () {
+                  showDeleteDialog(context);
+                },
+              )
+            ],
+          ),
+          body: Container(
+            color: colors[color],
+            child: Column(
+              children: <Widget>[
+                PriorityPicker(
+                  selectedIndex: 3 - note.priority,
+                  onTap: (index) {
+                    isEdited = true;
+                    note.priority = 3 - index;
+                  },
+                ),
+                ColorPicker(
+                  selectedIndex: note.color,
+                  onTap: (index) {
+                    setState(() {
+                      color = index;
+                    });
+                    isEdited = true;
+                    note.color = index;
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: titleController,
+                    maxLength: 255,
+                    style: Theme.of(context).textTheme.bodyText2,
+                    onChanged: (value) {
+                      updateTitle();
+                    },
+                    decoration: const InputDecoration.collapsed(
+                      hintText: 'Title',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 10,
+                      maxLength: 255,
+                      controller: descriptionController,
+                      style: Theme.of(context).textTheme.bodyText1,
+                      onChanged: (value) {
+                        updateDescription();
+                      },
+                      decoration: const InputDecoration.collapsed(
+                        hintText: 'Description',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  void showDiscardDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          title: Text(
+            "Discard Changes?",
+            style: Theme.of(context).textTheme.bodyText2,
+          ),
+          content: Text("Are you sure you want to discard changes?",
+              style: Theme.of(context).textTheme.bodyText1),
+          actions: <Widget>[
+            TextButton(
+              child: Text("No",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText2
+                      .copyWith(color: Colors.purple)),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text("Yes",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText2
+                      .copyWith(color: Colors.purple)),
+              onPressed: () {
+                Navigator.of(context).pop();
+                moveToLastScreen();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showEmptyTitleDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          title: Text(
+            "Title is empty!",
+            style: Theme.of(context).textTheme.bodyText2,
+          ),
+          content: Text('The title of the note cannot be empty.',
+              style: Theme.of(context).textTheme.bodyText1),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Okay",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText2
+                      .copyWith(color: Colors.purple)),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showDeleteDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          title: Text(
+            "Delete Note?",
+            style: Theme.of(context).textTheme.bodyText2,
+          ),
+          content: Text("Are you sure you want to delete this note?",
+              style: Theme.of(context).textTheme.bodyText1),
+          actions: <Widget>[
+            TextButton(
+              child: Text("No",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText2
+                      .copyWith(color: Colors.purple)),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text("Yes",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText2
+                      .copyWith(color: Colors.purple)),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _delete();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void moveToLastScreen() {
+    Navigator.pop(context, true);
+  }
+
+  void updateTitle() {
+    isEdited = true;
+    note.title = titleController.text;
+  }
+
+  void updateDescription() {
+    isEdited = true;
+    note.description = descriptionController.text;
+  }
+
+  // Save data to database
+  void _save() async {
+    moveToLastScreen();
+
+    note.date = DateFormat.yMMMd().format(DateTime.now());
+
+    if (note.id != null) {
+      await helper.updateNote(note);
+    } else {
+      await helper.insertNote(note);
+    }
+  }
+
+  void _delete() async {
+    await helper.deleteNote(note.id);
+    moveToLastScreen();
+  }
+}
