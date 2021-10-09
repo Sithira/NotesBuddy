dc770f7b19a12db2947b635af0c11e50ccda4858
Sithira Munasinghe <sithiraac@gmail.com>
2021-10-09 22:07:52 +0530

    Added backend

---
+++ backend/src/main/java/me/sithiramunasinghe/notesbuddy/notesbuddy_backend/NotesbuddyBackendApplication.java
@@ -0,0 +1,13 @@
+package me.sithiramunasinghe.notesbuddy.notesbuddy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesbuddyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesbuddyBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/me/sithiramunasinghe/notesbuddy/notesbuddy_backend/NotesbuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sithiramunasinghe.notesbuddy.notesbuddy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesbuddyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
